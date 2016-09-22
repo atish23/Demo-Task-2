35c0722ff4f60f4211c526f89d67140337d1cd8a
Atish <amaske@delaplex.in>
2016-09-22 13:20:19 +0000

    change

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -2,9 +2,20 @@
 $(document).ready(function(){
 $('ul.first').bsPhotoGallery({
   "classes" : "col-lg-2 col-md-4 col-sm-3 col-xs-4 col-xxs-12",
-  "hasModal" : true,
+  "hasModal" : false,
   // "fullHeight" : false
 });
+   $('li img').on('click',function(){
+        var src = $(this).attr('src');
+        var img = '<img src="' + src + '" class="img-responsive"/>';
+        $('#myModal').modal();
+        $('#myModal').on('shown.bs.modal', function(){
+            $('#myModal .modal-body').html(img);
+        });
+        $('#myModal').on('hidden.bs.modal', function(){
+            $('#myModal .modal-body').html('');
+        });
+   });
 });
 
 $(function() {
